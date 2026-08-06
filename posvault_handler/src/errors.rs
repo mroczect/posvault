4dd1b71f6efbff96872be680b7f3714f04df6b89
mroczect <mroczect@proton.me>
2026-08-06 22:31:01 +0700

    feat(handler): add Vctrl and Tree error conversions

--- posvault_handler/src/errors.rs
+++ posvault_handler/src/errors.rs
@@ -1,3 +1,4 @@
+use libvctrl::*;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -32,8 +33,20 @@ pub enum PosVaultError {
     #[error("I/O error: {0}")]
     Io(#[from] std::io::Error),
 
+    #[error("Vctrl error: {0}")]
+    Vctrl(#[from] libvctrl::error::VctrlError),
+
     #[error("External error: {0}")]
     External(Box<dyn std::error::Error + Send + Sync>),
+
+    #[error("Tree error: {0}")]
+    Tree(#[from] TreeError),
+}
+
+impl From<&str> for PosVaultError {
+    fn from(s: &str) -> Self {
+        PosVaultError::InvalidInput(s.to_owned())
+    }
 }
 
 pub type Result<T> = std::result::Result<T, PosVaultError>;
